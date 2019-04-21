810472f6c0e222cfd88c59462eeccb3427cba60d
Louis Varley <32775301+li-louisvarley@users.noreply.github.com>
2019-04-21 23:44:08 +0100

    Update updater.py

--- RaspApi/services/updater.py
+++ RaspApi/services/updater.py
@@ -36,3 +36,25 @@ class updateService(object):
                 for file in z.namelist():
                     if file.startswith('RaspApi-master/'):
                         z.extract(file, workingDir)
+
+
+        root_src_dir = workingDir + "/RaspApi-master"
+        root_target_dir = workingDir
+
+        operation= 'move' # 'copy' or 'move'
+
+        for src_dir, dirs, files in os.walk(root_src_dir):
+            dst_dir = src_dir.replace(root_src_dir, root_target_dir)
+            if not os.path.exists(dst_dir):
+                os.mkdir(dst_dir)
+            for file_ in files:
+                src_file = os.path.join(src_dir, file_)
+                dst_file = os.path.join(dst_dir, file_)
+                if os.path.exists(dst_file):
+                    os.remove(dst_file)
+                if operation is 'copy':
+                    shutil.copy(src_file, dst_dir)
+                elif operation is 'move':
+                    shutil.move(src_file, dst_dir)
+
+
